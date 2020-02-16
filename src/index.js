542d9a884af6be245be6b33b998a7bb09f370760
lilihsu <a940139kevin@gmail.com>
2020-02-17 04:55:37 +0800

    import react video css

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import * as firebase from 'firebase';
 import PlayerControl from './PlayerControl.js';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import "../node_modules/video-react/dist/video-react.css";
 const firebaseConfig = {
   apiKey: "AIzaSyBsxrMsRiJAppnMX5uXUT5VtzvDFCXzjxA",
   authDomain: "baseball-ui.firebaseapp.com",
