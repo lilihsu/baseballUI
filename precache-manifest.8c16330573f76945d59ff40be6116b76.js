bd0c316f9c1919b27a1648171118c9d8e1d747a3
lilihsu <a940139kevin@gmail.com>
2020-02-17 04:26:17 +0800

    Updates

---
+++ precache-manifest.8c16330573f76945d59ff40be6116b76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "318f62d3e80c41dc78e0adc8c341a41c",
+    "url": "/baseballUI/index.html"
+  },
+  {
+    "revision": "8351e9aae31d14d1347f",
+    "url": "/baseballUI/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "8351e9aae31d14d1347f",
+    "url": "/baseballUI/static/js/2.f06d1a7e.chunk.js"
+  },
+  {
+    "revision": "bc82a7a92f697e60ec51f64853cfb7ed",
+    "url": "/baseballUI/static/js/2.f06d1a7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b09387fcfae6759b650e",
+    "url": "/baseballUI/static/js/main.8fe65843.chunk.js"
+  },
+  {
+    "revision": "093fd35ec783474f2777",
+    "url": "/baseballUI/static/js/runtime-main.e491d06b.js"
+  }
+]);
